07b1857d527b6879b0ace4d28443edaa71d56d0f
k2dums <norbu.paddu@gmail.com>
2022-01-27 21:11:13 +0530

    parameter prompt was added to setSpecification() with the addition of parameter prompt Created if prompt was  not -1 Updated some texts within the input for the device characteristics

--- Mobile.py
+++ Mobile.py
@@ -4,7 +4,6 @@ from DeviceNode import DeviceNode
 class Mobile(DeviceNode):
     #Here we are assuming the nominal voltage of the mobile device to be around 3.7
     __NOM_VOLTAGE:Final=3.7
-    __deviceId=1
     def __init__(self) -> None:
         #the battery capacity eg 4000mAh
         self.__batteryCapacity=-1
@@ -21,7 +20,7 @@ class Mobile(DeviceNode):
         #Here we assume the Depth of Discharge for the battery is 100%
 
     #Function computes the present load of the device and the power usage of the device in Watt
-    def __wattDuringLoad(self):
+    def __wattDuringLoad(self)->float:
         return (self.__batteryCapacity/100)* Mobile.__NOM_VOLTAGE
     
     def getConfig(self)->None:
@@ -31,24 +30,26 @@ class Mobile(DeviceNode):
         print("Battery Energy:",self.__batteryEnergy)
         print("\n\n")
     
-    def setSpecification(self,processingPower=2000,ram=4,memory=500,downloadRate=100,uploadRate=100,batteryCapacity=4000)->None:
-        self.__batteryCapacity=batteryCapacity
-        self.__batteryEnergy=self.__batteryCapacity
-        powerWatt=self.__wattDuringLoad()
-        super().__init__(Mobile.__deviceId,processingPower,ram,memory,downloadRate,uploadRate,powerWatt)
-        Mobile.__deviceId+=1
+    def setSpecification(self,processingPower=2000,ram=4,memory=500,downloadRate=100,uploadRate=100,batteryCapacity=4000,prompt:int=-1)->None:
+        powerWatt=0
+        if prompt !=-1:
+            processingPower=int(input("Give the processing power in MIPs:"))
+            if( int(input("Would you like to give more details for the device ,1 for yes else 0:"))==1 ):
+                ram=int(input("Give the RAM size in GB:"))
+                memory=int(input("Give the Memory space in GB:"))
+                downloadRate=int(input("Give the download Rate in Mbps:"))
+                uploadRate=int(input("Give the Upload Rate in Mbps"))
+                batteryCapacity=int(input("Give the battery capacity in MAh:"))
+                powerWatt=self.__wattDuringLoad()
+           
+        else:
+            self.__batteryCapacity=batteryCapacity
+            self.__batteryEnergy=self.__batteryCapacity
+            powerWatt=self.__wattDuringLoad()
+        super().__init__(processingPower,ram,memory,downloadRate,uploadRate,powerWatt)
+
+       
         
-        
-mobile1=Mobile()
-mobile1.setSpecification()
-mobile2=Mobile()
-mobile2.setSpecification()
-mobile1.drainBattery(100)
-
-
-mobile1.getConfig()
-mobile2.getConfig()
-
 
 
 
