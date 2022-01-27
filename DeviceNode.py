91a2a279135d20a3053fae565ca44ff8da7bda88
k2dums <norbu.paddu@gmail.com>
2022-01-27 21:14:51 +0530

    updated the setSpecifcation() and includes new parameter prompt created if block using prompt updated the input text within device chracteristics added new instance variable called isStation

--- DeviceNode.py
+++ DeviceNode.py
@@ -2,6 +2,7 @@ from typing import Final
 
 
 class DeviceNode:
+    __deviceId=1
     CREATED:Final[int]=0
     READY:Final[int]=1
     QUEUED:Final[int]=1
@@ -10,9 +11,9 @@ class DeviceNode:
     PAUSED:Final[int]=7
     RESUMED:Final[int]=8
     FAILED_RESOURCE_UNAVAILABLE:Final[int]=9
-    def __init__(self,idIs,pp,ram,memory,downloadR,uploadR,powerWatt) -> None:
+    def __init__(self,pp=2000,ram=4,memory=500,downloadR=100,uploadR=100,powerWatt=-1,isStation="Unknown") -> None:
         #this id correponds to the device\node 
-        self.__deviceId:int=idIs
+        self.__deviceId:int=DeviceNode.setDeviceId()
         #Milion intructions per sec
         self.__processingPower:int=pp
         #the length in Million instruction
@@ -33,6 +34,24 @@ class DeviceNode:
         self.__resourceList:list=[]
         #the usage of the device
         self.__powerWatt:int=powerWatt
+        #whether the device is station or not
+        self.__isStation=isStation
+    
+    def setDeviceId():
+        DeviceNode.__deviceId+=1
+        return DeviceNode.__deviceId-1
+    
+    def setSpecification(self,processingPower=2000,ram=4,memory=500,downloadRate=100,uploadRate=100,powerWatt=-1,prompt:int=-1):
+        if prompt !=-1:
+            processingPower=int(input("Give the processing power in MIPs:"))
+            if( int(input("Would you like to give more details for the device ,1 for yes else 0:"))==1 ):
+                ram=int(input("Give the RAM size in GB:"))
+                memory=int(input("Give the Memory space in GB:"))
+                downloadRate=int(input("Give the download Rate Mbps:"))
+                uploadRate=int(input("Give the Upload Rate Mbps:"))
+                powerWatt=int(input("Watt usage of the device:"))
+        self.__init__(processingPower,ram,memory,downloadRate,uploadRate,powerWatt,False)
+
         
     def getConfig(self)->None:
         print("Device id =",self.__deviceId)
