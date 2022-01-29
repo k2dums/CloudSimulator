ba769aca322053a594d2091a3d8c1cbcf8da7007
k2dums <norbu.paddu@gmail.com>
2022-01-29 09:44:23 +0530

    Modification of init(),setSpecification()
    init() simply calls super().init
    setSpecification() removed parameter prompt and use of setter and getter
    created setBatteryCapacity()

--- Mobile.py
+++ Mobile.py
@@ -6,13 +6,15 @@ class Mobile(DeviceNode):
     __NOM_VOLTAGE:Final=3.7
     def __init__(self) -> None:
         #the battery capacity eg 4000mAh
-        self.__batteryCapacity=-1
+        self.__batteryCapacity=4000
         #the eneergy of the battery stored
-        self.__batteryEnergy=-1
+        self.__batteryEnergy=self.__batteryCapacity
+        super().__init__()
+    
+ 
         
     
-    def getBatteryEnergy(self):
-        return self.__batteryEnergy
+    
     
     #Function that drain the batery energy based on time
     def drainBattery(self,time:float=1) ->None:
@@ -23,36 +25,39 @@ class Mobile(DeviceNode):
     def __wattDuringLoad(self)->float:
         return (self.__batteryCapacity/100)* Mobile.__NOM_VOLTAGE
     
+   
+    
+    def setSpecification(self,processingPower=2000,ram=4,memory=500,downloadRate=100,uploadRate=100,batteryCapacity=4000)->None:
+        powerWatt=self.__wattDuringLoad
+        processingPower=int(input("Give the processing power in MIPs:"))
+        if( int(input("Would you like to give more details for the device ,1 for yes else 0:"))==1 ):
+            ram=int(input("Give the RAM size in GB:"))
+            memory=int(input("Give the Memory space in GB:"))
+            downloadRate=int(input("Give the download Rate in Mbps:"))
+            uploadRate=int(input("Give the Upload Rate in Mbps"))
+            batteryCapacity=int(input("Give the battery capacity in MAh:"))
+            powerWatt=self.__wattDuringLoad()
+        self.__setProcessingPower(processingPower)
+        self.__setRam(ram)
+        self.__setMemory(memory)
+        self.__setDownloadRate(downloadRate)
+        self.__setUploadRate(uploadRate)
+        self.__setPowerWatt(powerWatt)
+        self.__setBatteryCapacity(batteryCapacity)
+
+    def getBatteryCapcity(self):
+        return self.__batteryCapacity
+    def getBatteryEnergy(self):
+        return self.__batteryEnergy 
+    def __setBatteryCapacity(self,bc:int):
+        self.__batteryCapacity=bc
+        self.__batteryEnergy=bc
     def getConfig(self)->None:
         super().getConfig()
         print("Battery Capacity:",self.__batteryCapacity)
         print("Nominal Voltage:",Mobile.__NOM_VOLTAGE)
         print("Battery Energy:",self.__batteryEnergy)
         print("\n\n")
-    
-    def setSpecification(self,processingPower=2000,ram=4,memory=500,downloadRate=100,uploadRate=100,batteryCapacity=4000,prompt:int=-1)->None:
-        powerWatt=0
-        if prompt !=-1:
-            processingPower=int(input("Give the processing power in MIPs:"))
-            if( int(input("Would you like to give more details for the device ,1 for yes else 0:"))==1 ):
-                ram=int(input("Give the RAM size in GB:"))
-                memory=int(input("Give the Memory space in GB:"))
-                downloadRate=int(input("Give the download Rate in Mbps:"))
-                uploadRate=int(input("Give the Upload Rate in Mbps"))
-                batteryCapacity=int(input("Give the battery capacity in MAh:"))
-                self.__batteryCapacity=batteryCapacity
-                powerWatt=self.__wattDuringLoad()
-                
-           
-        else:
-            self.__batteryCapacity=batteryCapacity
-            self.__batteryEnergy=self.__batteryCapacity
-            powerWatt=self.__wattDuringLoad()
-        super().__init__(processingPower,ram,memory,downloadRate,uploadRate,powerWatt)
-
-    def getBatteryCapcity(self):
-        return self.__batteryCapacity
-        
 
 
 
