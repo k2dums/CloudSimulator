749da88d2f0db3116818f0667e54fe881c9ca0f9
k2dums <norbu.paddu@gmail.com>
2022-01-29 09:47:43 +0530

    deviceId value,setters,setSpecificaition(),init()
    resolved the deviceId value being set multiples of 2
    
    setters created for processingPower,ram,memory,downloadRate,uploadRate
    etc
    
    setSpecification() modified to resolve deviceId as it calls init which
    increments the value of deviceId (as init sets the deviceId and
    increments it).Also the use of setters in the functions.Removed
    parameter prompt from the function
    
    removed the instance variable isStation from the class

--- DeviceNode.py
+++ DeviceNode.py
@@ -1,8 +1,10 @@
 from typing import Final
 
+from sympy import memoize_property
+
 
 class DeviceNode:
-    __deviceId=1
+    __DEVICEID=1
     CREATED:Final[int]=0
     READY:Final[int]=1
     QUEUED:Final[int]=1
@@ -11,9 +13,9 @@ class DeviceNode:
     PAUSED:Final[int]=7
     RESUMED:Final[int]=8
     FAILED_RESOURCE_UNAVAILABLE:Final[int]=9
-    def __init__(self,pp=2000,ram=4,memory=500,downloadR=100,uploadR=100,powerWatt=-1,isStation="Unknown") -> None:
+    def __init__(self,pp=2000,ram=4,memory=500,downloadR=100,uploadR=100,powerWatt=-1) -> None:
         #this id correponds to the device\node 
-        self.__deviceId:int=DeviceNode.setDeviceId()
+        self.__deviceId:int=DeviceNode.__setDeviceId()
         #Milion intructions per sec
         self.__processingPower:int=pp
         #the length in Million instruction
@@ -35,34 +37,38 @@ class DeviceNode:
         #the usage of the device
         self.__powerWatt:int=powerWatt
         #whether the device is station or not
-        self.__isStation=isStation
     
-    def setDeviceId():
-        DeviceNode.__deviceId+=1
-        return DeviceNode.__deviceId-1
     
-    def setSpecification(self,processingPower=2000,ram=4,memory=500,downloadRate=100,uploadRate=100,powerWatt=-1,prompt:int=-1):
-        if prompt !=-1:
-            processingPower=int(input("Give the processing power in MIPs:"))
-            if( int(input("Would you like to give more details for the device ,1 for yes else 0:"))==1 ):
-                ram=int(input("Give the RAM size in GB:"))
-                memory=int(input("Give the Memory space in GB:"))
-                downloadRate=int(input("Give the download Rate Mbps:"))
-                uploadRate=int(input("Give the Upload Rate Mbps:"))
-                powerWatt=int(input("Watt usage of the device:"))
-        self.__init__(processingPower,ram,memory,downloadRate,uploadRate,powerWatt,False)
+    def __setDeviceId() ->int:
+        DeviceNode.__DEVICEID +=1
+        return (DeviceNode.__DEVICEID-1)
+    
+    def setSpecification(self,processingPower=2000,ram=4,memory=500,downloadRate=100,uploadRate=100,powerWatt=-1):
+        processingPower=int(input("Give the processing power in MIPs:"))
+        if( int(input("Would you like to give more details for the device ,1 for yes else 0:"))==1 ):
+            ram=int(input("Give the RAM size in GB:"))
+            memory=int(input("Give the Memory space in GB:"))
+            downloadRate=int(input("Give the download Rate in Mbps:"))
+            uploadRate=int(input("Give the Upload Rate in Mbps:"))
+            powerWatt=int(input("Watt usage of the device:"))
+        self.__setProcessingPower(processingPower)
+        self.__setRam(ram)
+        self.__setMemory(memory)
+        self.__setDownloadRate(downloadRate)
+        self.__setUploadRate(uploadRate)
+        self.__setPowerWatt(powerWatt)
 
         
     def getConfig(self)->None:
-        print("Device id =",self.__deviceId)
-        print("Processing Power:",self.__processingPower)
-        print("Instruction Length:",self.__instructionLength)
-        print("Ram:",self.__ram)
-        print("Memory:",self.__memory)
-        print("Download Rate:",self.__downloadRate)
-        print("Upload Rate:",self.__uploadRate)
-        print("Resource List:",self.__resourceList)
-        print("PowerWatt:",self.__powerWatt)
+        print("Device id =",self.getDeviceId())
+        print("Processing Power:",self.getProcessingPower())
+        print("Instruction Length:",self.getInstructionLength())
+        print("Ram:",self.getRam())
+        print("Memory:",self.getMemory())
+        print("Download Rate:",self.getDownloadRate())
+        print("Upload Rate:",self.getUploadRate())
+        print("Resource List:",self.getResourceList())
+        print("PowerWatt:",self.getPowerWatt())
     
     def getDeviceId(self):
         return self.__deviceId
@@ -86,5 +92,26 @@ class DeviceNode:
         return self.__resourceList
     def getPowerWatt(self):
         return self.__powerWatt
-    def getIsStation(self):
-        return self.__isStation
+
+   
+    def __setProcessingPower(self,pp:int):
+        self.__processingPower=pp
+    def __setInstructionLength(self,il):
+        self.__instructionLength=il
+    def __setRam(self,ram:int):
+        self.__ram=ram
+    def __setMemory(self,m:int):
+        self.__memory=m
+    def __setDownloadRate(self,dr:int):
+        self.__downloadRate=dr
+    def __setUploadRate(self,ur:int):
+        self.__uploadRate=ur
+    def __setExecutionStartTime(self,est):
+        self.__execStartTime=est
+    def __setFinishTime(self,ft):
+        self.__finishTime=ft
+    def __setResourceList(self,rl):
+        self.__resourceList=rl
+    def __setPowerWatt(self,pw:float):
+        self.__powerWatt=pw
+
