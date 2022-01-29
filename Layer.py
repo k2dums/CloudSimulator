dec87929107cee24ff242788a6ddbc3e094662c7
k2dums <norbu.paddu@gmail.com>
2022-01-29 10:00:47 +0530

    Allows stations be part of a cluster connection between clusters allowed
    eachDeviceSpecification() and standardSpecification() modified to take
    the stations in a cluster ,datastructure for self.deviceSpec_inLayer
    changed to accomodate stations into the cluster
    
    inputData() now asks the the no of stations within a cluster
    connectionMatrix now makes connection between cluster to cluster in a
    layer
    
    printLayerSummary() displays type of the device whether
    mobile,non mobile,station

--- Layer.py
+++ Layer.py
@@ -1,10 +1,8 @@
-#GraphData not suitable name and needs to be changed
-#Can be seen as a a layer and the clusters in that Layer
-#Naming can be Layer.py
-#Connection matrix can be removed since the connection will be handled by the network.py
+#Layer Connection matrix  is used for the connection between clusters 
 import numpy as np
-from Mobile import Mobile
 from DeviceNode import DeviceNode
+from Mobile import Mobile
+from Station import Station
 class Layer:
     __LAYER_ID=0
     def __init__(self) -> None:
@@ -20,87 +18,83 @@ class Layer:
         print("\n")
         print(f"Layer-{self.__layerId} Config")
         self.__clusters=int(input("Give the number of Clusters:"))
-        self.__station=int(input("Give the number of stations:"))
-        self.__devicePerCluster=np.array([])
-        #Here we are allocating devices per cluster
+        self.__devicePerCluster=[]
+        #Here we are allocating devices  and stations per cluster
         for i in range(self.__clusters):
             devices=int(input(f"For Cluster {i} give the number of devices:"))
-            self.__devicePerCluster=np.append(self.__devicePerCluster,devices)
-        #Now we need connection ,see if they are connected
-        #By default a cluster cannot connect directly to another cluster
-        #Here the connection_matrix is simply to find to connection of station to cluster
-        self.__connectionMatrix=np.zeros((self.__station,self.__clusters))
-        print("If Connection exist give 1 else 0")
-        for i in range(self.__station):
-            for j in range(self.__clusters):
-                print(f'Connection between station {i} and cluster {j}:')
-                value=-1
-                while value!=0 and value!=1 :
-                    value=int(input("Connection value=:"))
-                    if value!=0 and value!=1:
-                        print("Error with the connection value try again:")
-                self.__connectionMatrix[i][j]=value
+            stations=int(input(f"For Cluster {i} give the number of stations:"))
+            self.__devicePerCluster.append([devices,stations])
 
+        #Now we need connection ,see if they are connected
+        #Here the connection_matrix is simply to find to connection of clsuter to cluster
+        # self.__connectionMatrix=np.zeros((self.__clusters,self.__clusters))
+        # print("If Connection exist give 1 else 0")
+        # for i in range(self.__clusters):
+        #     for j in range(self.__clusters):
+        #         if i==j:
+        #             continue
+        #         print(f'Connection between cluster {i} and cluster {j}:')
+        #         value=-1
+        #         while value!=0 and value!=1 :
+        #             value=int(input("Connection value=:"))
+        #             if value!=0 and value!=1:
+        #                 print("Error with the connection value try again:")
+        #         self.__connectionMatrix[i][j]=value
+        # self.__devicePerCluster=np.array(self.__devicePerCluster)
+        # print(self.__devicePerCluster)
+
+
+    #Overall DataStructure  for an entity in self.__deviceSpec_inLayer
+     # [   [ [device] [station]  ]        ]
     def __eachDeviceSpecification(self):
         #This is a list containing the devices specifications for each cluster
-        #Here we pass in either mobile object or DeviceNode object
         self.__deviceSpec_inLayer=[]
-        #This keeps a track of the devices in each cluster
-         # clusterDeviceInfo=[   [cluster1]   ,[cluster2] .....[clustern]]
-        #if this is cluster1 inside clusterDeviceInfo Then cluster1= [device1_spec,device2_spec,device3_spec]
-        #Overall DataStructure  for a cluster entity
-         # [   [[],[],[]]     ]
-        
-        #Below  for when in a cluster there can be a station and devices
-        # for cluster in self.__devicePerCluster:
-        #     #This if for the normal devices(setting specification)
-        #     specEachDevice=[]
-        #     specEachStation=[]
-        #     for i in  range(int(cluster[0])):
-        #         if input("Is the device mobile or not.Give 1 for yes,else 0") ==1:
-        #             specEachDevice.append(Mobile().setSpecification())
-        #         else:
-        #             specEachDevice.append(DeviceNode().setSpecification())
-        #     #This is for the station(setting specfication)
-        #     for i in range(int(cluster[1])):
-        #         specEachDevice.append(DeviceNode().setSpecification())
-        #     #
-        #     deviceSpec_inCluster.append([specEachDevice,specEachStation])
-
-        #Below when there can be only devices
-        for  cluster,n_Devices in  enumerate(self.__devicePerCluster):
-            #This variable is used to store the device specification per cluster
-            specEachDevice_inCluster=[]
-            print(f"\nGiving the device specfication for cluster {cluster}")
-            for deviceno,n in enumerate(range(int(n_Devices))):
-                print(f"For device {deviceno},Cluster {cluster}")
-                if int(input("Is the device mobile or not.Give 1 for yes,else 0:")) ==1:
+    
+        #if prompt=1 it tells to display and no input the device specification
+        #else when prompt!=1 no prmopt and no input for the device specification
+        for cluster in self.__devicePerCluster:
+            #This if for the normal devices(setting specification)
+            specEachDevice=[]
+            specEachStation=[]
+            for i in  range(int(cluster[0])): #cluster0 represents the deivces
+                if input("Is the device mobile or not.Give 1 for yes,else 0") ==1:
                     obj=Mobile()
-                    obj.setSpecification(prompt=1)
-                    specEachDevice_inCluster.append(obj)
+                    obj.setSpecification()
+                    specEachDevice.append(obj)
                 else:
                     obj=DeviceNode()
-                    obj.setSpecification(prompt=1)
-                    specEachDevice_inCluster.append(obj)
-            #Appending of specEachDevice_inCluster forms the layer
-            self.__deviceSpec_inLayer.append([specEachDevice_inCluster])
+                    obj.setSpecification()
+                    specEachDevice.append(obj)
 
+            #This is for the station(setting specfication)
+            for i in range(int(cluster[1])):#cluster1 represnts the stations
+                obj=Station()
+                obj.setSpecification()
+                specEachDevice.append(obj)
+            specEachCluster=[specEachDevice,specEachStation]
+            self.__deviceSpec_inLayer.append(specEachCluster)
+        print(self.__deviceSpec_inLayer)
 
+      
 
     #We will add standard specification for the devices and station
     #More info can be seen from eachDeviceSpecification()
     #All the devices here are assumed to be non-mobile
     def __standardSpecification(self):
         self.__deviceSpec_inLayer=[]
-        for  n_Devices in  self.__devicePerCluster:
-            #This variable is used to store the device specification per cluster
-            specEachDevice_inCluster=[]
-            for n in range(int(n_Devices)):
-                obj=DeviceNode()
-                obj.setSpecification()
-                specEachDevice_inCluster.append(obj)
-            #Appending of specEachDevice_inCluster forms the layer
-            self.__deviceSpec_inLayer.append([specEachDevice_inCluster])
+        for cluster in self.__devicePerCluster:
+            print(f"cluster {cluster}")
+            specEachDevice=[]
+            specEachStation=[]
+            for devices in range(cluster[0]):#devices
+                    obj=DeviceNode()
+                    specEachDevice.append(obj)
+            for stations in range(cluster[1]):#stations
+                    obj=Station()
+                    specEachStation.append(obj)
+            specEachCluster=[specEachDevice,specEachStation]
+            self.__deviceSpec_inLayer.append(specEachCluster)
+        print(self.__deviceSpec_inLayer)
 
     #This reuqests from user to set standard specification or set each specifcation
     def specificationRequest(self):
@@ -123,7 +117,7 @@ class Layer:
     def printLayerSummary(self):
         print("\n")
         print(f"Layer-{self.__layerId} Summary".center(100," "))
-        print(f"Device Id".center(20," ")+"|"+f"ProcessingPower".center(20," ")+"|"+f"WattUsage".center(20," ")+"|"+f"Battery".center(20," ")+"|")
+        print(f"Device Id".center(20," ")+"|"+f"Processing Power".center(20," ")+"|"+f"WattUsage".center(20," ")+"|"+f"Battery".center(20," ")+"|"+f"Type".center(20," "))
         # for cluster in enself.__deviceSpec_inLayer:
         #     print("Cluster Value",cluster)
         #     print("cluster",type(cluster))
@@ -145,9 +139,15 @@ class Layer:
                     watt:str=f'{obj.getPowerWatt()}'.center(20," ")
                     if isinstance(obj,Mobile):
                         battery=f'{obj.getBatteryCapcity()}'.center(20," ")
+                        type="Mobile"
                     else:
                         battery="None".center(20," ")
-                    print(f"{id}|{pp}|{watt}|{battery}|")
+                        if isinstance(obj,Station):
+                            type="Station"
+                        elif isinstance(obj,DeviceNode):
+                            type="Non Mobile"
+                    type=type.center(20," ")
+                    print(f"{id}|{pp}|{watt}|{battery}|{type}")
             
 
 
@@ -186,3 +186,7 @@ class Layer:
         #Need to check the shape of matrix if station and cluster =2,3 but valueMatrix shape=3,3
         self.__connectionMatrix=valueMatrix
     
+
+layer=Layer()
+layer.specificationRequest()
+layer.printLayerSummary()
