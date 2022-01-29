7a74bf515bf8589005cd9a3442e8412681661cd4
k2dums <norbu.paddu@gmail.com>
2022-01-29 11:25:09 +0530

    Added layerId as instance variable to the class Network

--- Network.py
+++ Network.py
@@ -5,18 +5,21 @@ from Layer import Layer
 import numpy as np
 class Network:
     __NETWORK_ID=0
+    #The layerid needs to be passed during the creation of a layaer and updated likewise
     def __init__(self) -> None:
         self.__networkId=Network.__NETWORK_ID
         self.__networkLayers=np.array([])
+        self.__layerId=0
         Network.__NETWORK_ID+=1
     
     def createLayer(self):
-        layer=Layer()
+        layer=Layer(self.getLayerId())
+        self.__updateLayerId()
         layer.specificationRequest()
         self.__networkLayers=np.append(self.__networkLayers,layer)
     
     def printAllLayerSummary(self):
-        print(f"All Layer Summary,Network {self.__networkId}".center(100," "))
+        print(f"All Layer Summary-Network {self.__networkId}".center(100," "))
         for layer in self.__networkLayers:
             assert isinstance(layer,Layer)
             layer.printLayerSummary()
@@ -30,6 +33,13 @@ class Network:
     
     def makeLayerConnections(self):
         pass
+    
+    def getLayerId(self)->int:
+        return self.__layerId
+    
+    def __updateLayerId(self)->None:
+        self.__layerId+=1
+        
         
 
 
