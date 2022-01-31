0a6b62a2a895eba11ffa80ba27559d72c020daba
k2dums <norbu.paddu@gmail.com>
2022-02-01 00:22:32 +0530

    Network class no longer handles the connection
    The making of connection is now handled by NetworkConnection
    
    makeLayerConnection() creates a matrix which stores the
    NetworkConnection object
    
    Added comments for the use of various variables
    
    printLayerSummary() removed the display as it is redudant as is also
    printed by the Layer class function which it calls (printLayerSummary)

--- Network.py
+++ Network.py
@@ -1,14 +1,19 @@
 #This class keeps a track of all the layer created 
 #The layer object manipulated here
 #This class also makes the connection between the the cluster of the a layer and cluster of different layer
+from NetworkConnection import NetworkConnection
 from Layer import Layer
 import numpy as np
 class Network:
+    #Statiac variable to track the network id
     __NETWORK_ID=0
     #The layerid needs to be passed during the creation of a layaer and updated likewise
     def __init__(self) -> None:
+        #This sets the network id of the network data structure=[Layer object,Layer object ]
         self.__networkId=Network.__NETWORK_ID
+        #This keeps a track of the layers in the network
         self.__networkLayers=np.array([])
+        #this keeps a track of the layerid , here it is important to pass it to the Layer object and value is incremented in the Network Class
         self.__layerId=0
         Network.__NETWORK_ID+=1
     
@@ -25,21 +30,48 @@ class Network:
             layer.printLayerSummary()
     
     def printLayerSummary(self,n=0):
-        print(f"Layer {n} Summary".center(100," "))
         for i,layer in enumerate(self.__networkLayers):
             if i ==n:
                 assert isinstance(layer,Layer)
                 layer.printLayerSummary()
     
     def makeLayerConnections(self):
-        pass
+        print(f"\nConnection Specification for the Network {self.__networkId}")
+        #This is for the connection between the cluster layer of a layer to the cluster layer of another layer
+        self.__connectionMatrix=np.zeros((self.getNumberofLayers,self.getNumberofLayers,1))
+        for layerA in range(len(self.__networkLayers)):
+            for LayerB in range(layerA+1,len(self.__networkLayers)):
+                if layerA==LayerB:
+                    continue
+                connection=NetworkConnection(layerA,LayerB,self.__networkLayers[layerA,self.__networkLayers[LayerB]])
+                self.__connectionMatrix[layerA][LayerB][1]=connection
+
+
+
+      
+
+
+
     
     def getLayerId(self)->int:
         return self.__layerId
     
     def __updateLayerId(self)->None:
         self.__layerId+=1
+
+    def getnetworkLayers(self):
+        return self.__networkLayers
+    def getconnectionMatrix(self):
+        return self.__connectionMatrix
+    def getNumberofLayers(self):
+        return len(self.__networkLayers)
         
+network=Network()
+network.createLayer()
+network.createLayer()
+network.makeLayerConnections()
+print(network.getconnectionMatrix())
+
         
 
 
