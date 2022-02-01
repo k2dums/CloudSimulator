501eb3b3f48aaacbef1fc6f3e8d5ce0504c242e0
k2dums <norbu.paddu@gmail.com>
2022-02-01 22:58:34 +0530

    init() layer object had wrong refrence to the function getCluster() and used assertions for checking layer object
    makeConnection() changed the display prompts

--- NetworkConnection.py
+++ NetworkConnection.py
@@ -4,22 +4,24 @@ class NetworkConnection:
     def __init__(self,start,end,layerA_obj,layerB_obj) -> None:
         self.__start=start
         self.__end=end
+        assert isinstance(layerA_obj,Layer) and isinstance(layerB_obj,Layer)
         self.__layerA_obj=layerA_obj
         self.__layerB_obj=layerB_obj
-        self.__layerA_clusters=layerA_obj.getClusters()
-        self.__layerB_clusters=layerB_obj.getClusters()
-        self.__connection=np.zeros(self.__layerA_clusters,self.__layerB_clusters)
+        self.__layerA_clusters=layerA_obj.getCluster()
+        self.__layerB_clusters=layerB_obj.getCluster()
+        self.__connection=np.zeros((self.__layerA_clusters,self.__layerB_clusters))
         self.makeConnection()
    
+    #this function makes the connection between clusters of two layers
     def makeConnection(self):
-        print("Setting up connection betweene Layer {self.__start} and Layer {self.__end}")
+        print(f"\nSetting up connection betweene Layer {self.__start} and Layer {self.__end}")
         print("[y] for Connection else [n]")                  
         for clusterA in range(self.__layerA_clusters):
             for clusterB in range(self.__layerB_clusters):
-                print(f"Connection between Cluster {clusterA} and Cluster {clusterB}")
+                print(f"Connection between Layer{self.__start}-Cluster {clusterA} and  Layer{self.__end}-Cluster {clusterB}")
                 userInput="None"
                 while not(userInput == "y") and not(userInput == "n"):
-                    userInput=input(f"Connection [y] for connection else [n]:\n")
+                    userInput=input()
                     if not(userInput=='y') and not(userInput=='n'):
                         print("Error give a valid input")
                 assert userInput=='y' or userInput=='n'
@@ -37,3 +39,5 @@ class NetworkConnection:
         return self.__start
     def getTo(self)->None:
         return self.__end
+    def getNetworkConenction(self):
+        return self.__connection
