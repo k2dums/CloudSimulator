4beda9613724db23e70f2716ebc3915e4ee4cfa6
k2dums <norbu.paddu@gmail.com>
2022-02-17 21:54:27 +0530

    added comments

--- Station.py
+++ Station.py
@@ -1,3 +1,4 @@
+#This class is modelled to simply have a higher device specifications representing that of a high end commercial machine 
 from DeviceNode import DeviceNode
 class Station(DeviceNode):
     def __init__(self)->None:
