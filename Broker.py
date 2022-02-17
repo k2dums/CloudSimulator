4f914af52aac495826cbefc32cb72694d21ee1ca
k2dums <norbu.paddu@gmail.com>
2022-02-17 21:36:40 +0530

    Addition of Algorithm class,created functions removed two parameter __init__()
    Added comments on multiple functions
    Created a class variable for the state
    
    created functions:
    assignLinearResources():assings jobs linearly based on a pointer
    startsimulation() skeleton function created
    getActiveDeviceState() creates an instance variable with active devices
    activeDeviceSummary() prints the active devices with thier jobs
    allDeviceSummary() prints all the devices and their status
    resourceAllocationAlgorithm() allocation based on the algorithm
    setters and getters created,getResourceLength() and getInitialResourceList()
    isResourceEmpty() to check if the seq pointer is pointing outside the
    resources list
    
    class Algorithm:
    created FCFS() algorithm
    skeleton for COFFGA() and  CONFGA()

--- Broker.py
+++ Broker.py
@@ -1,25 +1,26 @@
 #This class is for allocating the resources and also is reponsible for the schdeduling algorithm used
 #It assigns the broker id to the device when allocating the resources to it
 #Concept of multithreading can be used to simulate,and say one sec processing makes simulation runs for 1 ms
+from psutil import swap_memory
+from sympy import N
 from Network import Network
 from DeviceNode import DeviceNode
 from Mobile import Mobile
 from Station import Station
 from Layer import Layer
+
+
 class Broker:
     __BROKER_ID=0
+    __state=["CREATED","READY","QUEUED","SUCCESS","FAILED","PAUSED","RESUMED","FAILED_RESOURCE_UNAVAILABLE"]
+    
     def __init__(self,network) -> None:
         self.__network=network
         self.__brokerId=Broker.__BROKER_ID
         self.__deviceId=0
         Broker.__BROKER_ID+=1
+        
     
-    def __init__(self,network,resourceList:list) -> None:
-        self.__network=network
-        self.__brokerId=Broker.__BROKER_ID
-        self.__deviceId=0
-        self.__resourceList=resourceList
-        Broker.__BROKER_ID+=1
 
     #A broker is responsible for a layer only
     # def __init__(self,network,resourceList:list,layerno) -> None:
@@ -30,27 +31,98 @@ class Broker:
     #     Broker.__BROKER_ID+=1
         
         
-    
-    def setResourceList(self,resourceList)->list:
+    #This sets the work that needs to be given to the Broker
+    def setResourceList(self,resourceList:list):
         self.__resourceList=resourceList
+        self.__seq_pointer=0
     
+    #This function sets the broker id for the device being used
     def __getBrokerDeviceId(self)->str:
         temp=f"#{self.__brokerId}_{self.__deviceId}"
         self.__deviceId+=1
         return temp
     
-    def getAllDeviceState():
-        pass
-    def getActiveDeviceState():
-        pass
-    def printActiveDeviceSummary():
-        pass
-    def printAllDeviceSumamry():
-        pass
-    def allocateResource():
-        pass
-    def allocateResource(algorithm):
-        pass
+    #This function is responsible for assigning work to the instance of DeviceNode,Station,Mobile
+    def assignLinearResources(self,device)->None:
+        if self.isResourceEmpty():
+            return 
+        assert isinstance(device,DeviceNode)
+        device.setResourceList(self.__resourceList[self.__seq_pointer])
+        self.__seq_pointer+=1
+        
+    def startSimulation(self):
+        if not(hasattr(self,self.__activeDevice)):
+            self.getActiveDeviceState()
+        time=0
+        for device in self.__activeDevice:
+            assert isinstance(device,DeviceNode)
+            work=device.getResourceList()
+            processingPower=device.getProcessingPower()
+            time+=work/processingPower
+        print("Total time taken:",time)
+        return time
+
+    # def getAllDeviceState():
+    #     pass
+    def getActiveDeviceState(self):
+        network=self.__network
+        assert isinstance(network,Network)
+        layers=network.getNetworkLayers()
+        self.__activeDevice=[]
+        for layer in layers:
+            assert isinstance(layer,Layer)
+            for cluster in layer.getdeviceSpec_inLayer():
+                for devices in cluster:
+                    for device in devices:
+                        assert isinstance(device,DeviceNode)
+                        if device.getStatus==DeviceNode.READY:
+                            self.__activeDevice.append(device)
+            
+    def ActiveDeviceStateSummary(self):
+        self.AllDeviceStateSummary(onlyActive=True)
+       
+
+    def AllDeviceStateSummary(self,onlyActive:bool=False):
+        network=self.__network
+        assert isinstance(network,Network)
+        layers=network.getNetworkLayers()
+        for layer in layers:
+            assert isinstance(layer,Layer)
+            print(f"Layer-{layer.getLayerid()} State Summary".center(100," "))
+            print(f"Device Id".center(20," ")+"|"+f"Type".center(20," ")+"|"+f"State".center(20," ")+"|"+f"Job".center(20," "))
+            clusters=layer.getdeviceSpec_inLayer()
+            for clusterno,cluster in enumerate(clusters):
+                print(f"Cluster-{clusterno}".center(75," "))
+                for devices in cluster:
+                    for device in devices:
+                        assert isinstance(device,DeviceNode)
+                        id:str=f'{device.getDeviceId()}'.center(20," ")
+                        status:int=device.getStatus()
+                        if onlyActive and (status==DeviceNode.FAILED or status==DeviceNode.FAILED_RESOURCE_UNAVAILABLE or status==DeviceNode.CREATED):
+                            continue
+                        status:str=Broker.__state[status].center(20," ")
+                        job=device.getResourceList()
+                        if len(job)==0:
+                            job="None".center(20," ")
+                        else :
+                            job=f"{job}".center(20," ")
+                        if isinstance(device,Mobile):
+                            type="Mobile"
+                        else:
+                            battery="None".center(20," ")
+                        if isinstance(device,Station):
+                            type="Station"
+                        elif isinstance(device,DeviceNode):
+                            type="Non Mobile"
+                        type=type.center(20," ")
+                        print(f"{id}|{type}|{status}|{job}")
+            
+
+    def resourceAllocationAlgorithm (self,algorithm=1):
+        algo=Algorithm(self.__network,self,algorithm)
+    
+        
+
     def startSimulation():
         pass
     def deviceBreakDown(deviceObj):
@@ -58,4 +130,61 @@ class Broker:
     def updateDeviceStatus(deviceObj):
         pass
     def simualationSummary():
+        pass
+
+  
+    def getResourceLength(self):
+        return self.__resourceList
+
+    def isResourceEmpty(self)->bool:
+        if self.__seq_pointer!=len(self.__resourceList):
+            return False
+        return True
+    def getInitialResourceList(self):
+        return self.__resourceList
+
+
+class Algorithm:
+
+    _FCFS:int=1
+    _COFFGA:int=2
+    _CONFGA:int=3
+
+    def __init__(self,network,broker,algorithm=1) -> None:
+        print(algorithm)
+        self.__network=network
+        assert isinstance(self.__network,Network)
+        self.__broker=broker
+        assert isinstance(self.__broker,Broker)
+        if algorithm == 1:
+            self.FCFS()
+        elif algorithm == 2:
+            self.COFFGA()
+        elif algorithm == 3:
+            self.CONFGA()
+
+        
+        
+    def FCFS(self):
+        print("Allocating using FCFS")
+        network=self.__network
+        assert isinstance(network,Network)  
+        broker=self.__broker
+        assert isinstance(broker,Broker)
+        no_Layers=network.getNumberofLayers()
+        layers=network.getNetworkLayers()
+        for layer in layers:
+            assert isinstance(layer,Layer)
+            clusters=layer.getdeviceSpec_inLayer()
+            for cluster in clusters:
+               for devices in cluster:
+                   for device in devices:
+                        assert isinstance(device,DeviceNode)
+                        broker.assignLinearResources(device)
+                        if broker.isResourceEmpty():
+                         return 
+    
+    def COFFGA():
+        pass
+    def CONFGA():
         pass
