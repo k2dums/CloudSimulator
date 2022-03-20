bebec66af10d3c527be13d2e5f4e680ffd7bde66
k2dums <norbu.paddu@gmail.com>
2022-03-21 01:23:32 +0530

    unwanted

--- Station.py
+++
@@ -1,5 +0,0 @@
-#This class is modelled to simply have a higher device specifications representing that of a high end commercial machine 
-from DeviceNode import DeviceNode
-class Station(DeviceNode):
-    def __init__(self)->None:
-        super().__init__(5000,32,1000,1000,1000,-1)
