26cc7ee818070af964dc43dd0068246018229fb9
k2dums <norbu.paddu@gmail.com>
2022-04-12 11:47:35 +0530

    added barplot

--- Graph.py
+++ Graph.py
@@ -1,6 +1,12 @@
 #This class is simply for showing the graph .for comparison,or show relativity with various parameters
+import matplotlib.pyplot as plt
 class Graph():
     def __init__(self) -> None:
         pass
-    def plotGraph():
-        pass
+    def plotBarGraph(names,values):
+        plt.bar(names,values,color='blue',width=0.4)
+        plt.xlabel=("Algorithms")
+        plt.ylabel=("Time Taken")
+        plt.title=("Time taken for different Algorithms")
+        plt.show()
+        
