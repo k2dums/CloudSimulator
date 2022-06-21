a082197157d80764498c112fd20485a14d37b621
k2dums <norbu.paddu@gmail.com>
2022-06-21 13:29:06 +0530

    added documentation to the module

--- Cluster.py
+++ Cluster.py
@@ -2,6 +2,11 @@ from DeviceNode import DeviceNode
 from Mobile import Mobile
 from Station import Station
 class Cluster:
+    """
+    Class is an abstraction of a cluster in a network\n
+    A cluster serves as a container for the number of devices within in \n
+    It has an attribute called weight , which is used in weighted Algorithms\n
+    """
     def __init__(self,id) -> None:
         self.id=id
         self.devices=[]
@@ -16,38 +21,58 @@ class Cluster:
         
 
     def addDevice(self):
+        """Adds device to the cluster\n
+            it returns the added device instance\\n
+        """
         obj=DeviceNode()
         self.devices.append(obj)
         return obj
     
     def addMobile(self):
+        """Adds mobile instance to the cluster\n
+           it returns the added mobile instance\\n        
+        """
         obj=Mobile()
         self.devices.append(obj)
         return obj
 
     def addStation(self):
+        """Adds station instance to the cluster\n
+             it returns the added station instance\\n
+        """
         obj=Station()
         self.devices.append(obj)
         return obj
 
     
     def makeDeviceConnections(self):
+        """Makes the device connection within a cluster\n
+            **Currently not functional just skeletion**\n
+        """
         self.connectionMatrix=[]
         pass
     def getNoOfDevice(self):
+        """Returns the number of devices in the cluster"""
         return len(self.devices)
     def getId(self):
+        """Returns the cluster id"""
         return self.id
     def getDevices(self):
+        """Returns the list device instances"""
         return self.devices
     def getWeight(self):
+        """Gets the weight of the cluster"""
         return self.weight
     def setWeight(self,weight):
+        """Sets the weight of the cluster"""
         self.weight=weight
         return self.weight
 
 
     def getUtilization(self):
+        """Returns the utilizaiotn of the cluster \n
+           Based on wether the device is occupied or not\n
+        """
         n=len(self.devices)
         if n==0:
             return -1
@@ -61,6 +86,9 @@ class Cluster:
         return utilization
     
     def isThereTask(self):
+        """Returns True if a device in the cluster 
+        has a task else False\n
+        """
         for device in self.devices:
             assert isinstance(device,DeviceNode)
             if device.getResourceList():
@@ -68,13 +96,15 @@ class Cluster:
         return False
 
     def getActiveDeviceNo(self):
+        """Returns the number of active devices in the cluster"""
         activeNo=0
         for device in self.getDevices():
             assert isinstance(device,DeviceNode)
-            if device.getStatus()==DeviceNode.READY:
+            if device.getStatus()==DeviceNode.READY or device.getStatus()==DeviceNode.PROCESSING or device.getStatus()==DeviceNode.SUCCESS:
                 activeNo+=1
         return activeNo
     def getNonActiveDevices(self):
+        """Returns teh number of non active devices in the cluster"""
         nonActive=[]
         for device in self.getDevices():
             assert isinstance(device,DeviceNode)
