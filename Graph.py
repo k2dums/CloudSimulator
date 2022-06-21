9c4efb938cb5fbd8e0170765ed1db893a8945557
k2dums <norbu.paddu@gmail.com>
2022-06-21 13:44:36 +0530

    Fixed LineGraph not showing properly by referring to older version

--- Graph.py
+++ Graph.py
@@ -1,12 +1,15 @@
-#This class is simply for showing the graph .for comparison,or show relativity with various parameters
+
 import matplotlib.pyplot as plt
 import numpy.random as np
 class Graph():
+    """
+    This class is simply for showing the graph for comparison,or show relativity with various parameters\n
+    """
     def __init__(self) -> None:
         pass
-    def plotBarGraph(names=[],values=[],title="Time taken for different Algorithms",xlabel="Algorithms",ylabel="Time Taken"):
+    def plotBarGraph(names=[],values=[],title="Time taken for different Algorithms",xlabel="Algorithms",ylabel="Time Taken (s)"):
         """
-        This plots the bar graph for time vs various algorithms in definite setup
+        This plots the bar graph for time vs various algorithms in definite setup\n
         """
 
         plt.bar(names,values,color='blue',width=0.4)
@@ -15,51 +18,53 @@ class Graph():
         plt.title(title)
         plt.show()
 
-    def linearGraph(legend_text,x:list,timeList:list[list[int]],title="Algorithms Time taken Vs Task Size",xlabel="No of Tasks",ylabel="Time Taken"):
+
+    def linearGraph(algoNames,taskSize:list,timeList:list[list[int]]):
         """
-        Plots the graph in a straight linear way
-        algoName:list[string] that will be printed as a label
-        x:list[int] values that will be printed in the  axis
-        timeList=[ [time taken by algo 1],[time taken by algo 2]  ]
+        Plots the graph in a straight linear way\n
+        algoName:list[string] that will be printed as a label\n
+        x:list[int] values that will be printed in the  axis\n
+        timeList=[ [time taken by algo 1],[time taken by algo 2]  ]\n
         """
-      
+        #timeList=[ [time taken by algo 1],[time taken by algo 2]  ]
         
-        if len(legend_text) != len(timeList):
+        if len(algoNames) != len(timeList):
             print("Error:Size of Algorithm mismatached time size")
 
 
-        for no,name in enumerate(legend_text):
+        for no,name in enumerate(algoNames):
             c=np.rand(3)
-            plt.plot(x,timeList[no],color=c,label=f'{name}')
+            plt.plot(taskSize,timeList[no],color=c,label=f'{name}')
           
 
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
-        plt.title(title)
+        plt.xlabel("No of Tasks")
+        plt.ylabel("Time Taken (s)")
+        plt.title("Algorithms Time taken Vs Task Size")
         plt.legend(bbox_to_anchor=(1,1.04), loc="upper right",fontsize=7)
         plt.show()
 
     
-    def lineGraph(legend_text=[], x:list=[],timeList:list[list[int]]=[],title="Algorithms Time taken Vs Task Size",xlabel="No of Tasks",ylabel="Time Taken"):
+    def lineGraph(algoNames,taskSize:list,timeList:list[list[int]]):
         """
-        Plots the graph 
-        legend_text:list[string] the text that will be printed as label
-        x:list[int] that values that will be in x axis
-        timeList=[ [time taken by algo 1],[time taken by algo 2]  ]
+         Plots the graph \n
+         legend_text:list[string] the text that will be printed as label\n
+         x:list[int] that values that will be in x axis\n
+         timeList=[ [time taken by algo 1],[time taken by algo 2]  ]\n
+         timeList=[ [time taken by algo 1],[time taken by algo 2]  ]
         """
-      
-        if len(legend_text) != len(timeList):
+       
+        if len(algoNames) != len(timeList):
             print("Error:Size of Algorithm mismatached time size")
+        
 
-
-        for no,name in enumerate(legend_text):
+        for no,name in enumerate(algoNames):
             c=np.rand(3)
             i=0
             j=0
             flag=False
-            while (i<(len(x)-1) and j< ( len(timeList[no]) -1) ):
-                p1_x=x[i]
-                p2_x=x[i+1]
+            while (i<(len(taskSize)-1) and j< ( len(timeList[no]) -1) ):
+                p1_x=taskSize[i]
+                p2_x=taskSize[i+1]
                 p1_y=timeList[no][j]
                 p2_y=timeList[no][j+1]
                 x=[p1_x,p2_x]
@@ -72,14 +77,11 @@ class Graph():
                 i+=1
                 j+=1
 
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
-        plt.title(title)
+        plt.xlabel("No of Tasks")
+        plt.ylabel("Time Taken (s)")
+        plt.title("Algorithms Time taken Vs Task Size")
         plt.legend(bbox_to_anchor=(1,1.04), loc="upper right",fontsize=7)
         plt.show()
-       
-
-
 
 
 
